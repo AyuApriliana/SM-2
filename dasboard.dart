d15c24d4d62baf55c58eacf963d14e4a6792eb12
Aldyoga <103398496+Aldyoga@users.noreply.github.com>
2023-05-07 20:42:34 +0700

    Add files via upload

---
+++ dasboard.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Dasboard extends StatefulWidget {
+  const Dasboard({super.key});
+
+  @override
+  State<Dasboard> createState() => _DasboardState();
+}
+
+class _DasboardState extends State<Dasboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column( 
+          children: [
+           const SizedBox(height: 50),
+        Center( 
+          child: Text('Hasil Laporan Jalan Rusak',
+          style: GoogleFonts.montserrat(fontSize: 24, fontWeight: FontWeight.bold)),
+        ),
+        SizedBox(
+          width: double.infinity,
+          height: 240,
+          child: Stack(
+            children: [
+              Card(
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                elevation: 10,
+                child: Column(children: [
+                  SizedBox(
+                    width: double.infinity,
+                    height: 150,
+                    child: Image.asset("assets/images/jalan-rusak.jpg"),
+                  )
+                ]
+                ),
+              ),
+               Positioned(bottom: 0, left: 10, 
+               child: SizedBox(
+                height: 70, 
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Jalan Sudirman',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold
+                    ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                      ),
+                    Row(children: [
+                      Icon(Icons.star, color: Colors.amber),
+                      Text('4,5'), 
+                    ],
+                    ),
+                  ],
+                ),),)
+            ],
+          ),
+        ),
+         SizedBox(
+          width: double.infinity,
+          height: 240,
+          child: Stack(
+            children: [
+              Card(
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                elevation: 10,
+                child: Column(children: [
+                  SizedBox(
+                    width: double.infinity,
+                    height: 150,
+                    child: Image.asset("assets/images/jalan-rusak.jpg"),
+                  )
+                ]
+                ),
+              ),
+               Positioned(bottom: 0, left: 10, 
+               child: SizedBox(
+                height: 70, 
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Jalan Diponegoro',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold
+                    ),
+                    ),
+                    // ignore: prefer_const_constructors
+                    SizedBox(
+                      width: 10,
+                      ),
+                    // ignore: prefer_const_literals_to_create_immutables
+                    Row(children: [
+                      Icon(Icons.star, color: Colors.amber),
+                      Text('5'), 
+                    ],
+                    ),
+                  ],
+                ),),)
+            ],
+          ),
+        )
+          ],
+          ),
+
+      ),
+    );
+  }
+}
