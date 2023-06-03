690d257aa3c6c30df76f0cf479f37db12694c716
AyuApriliana <aprilianaayu14@gmail.com>
2023-06-03 12:25:27 +0700

    laporan backend

--- lib/profile.dart
+++ lib/profile.dart
@@ -4,7 +4,7 @@ import 'package:laporan/dasboard.dart';
 import 'package:laporan/navbar.dart';
 
 class ProfilScreen extends StatelessWidget {
-  const ProfilScreen({super.key});
+  const ProfilScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,131 +13,98 @@ class ProfilScreen extends StatelessWidget {
         backgroundColor: Colors.blue,
         centerTitle: true,
         title: Text("Profile"),
-        actions: <Widget> [
-          TextButton(onPressed: () {},
-            child: Text(
-              "Edit",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.edit,
+              color: Colors.white,
             ),
           )
         ],
       ),
-      body:
-      Center(
+      body: SingleChildScrollView(
         child: Column(
-          children: <Widget> [
-            Container(
-              margin: EdgeInsets.only(bottom: 10),
-              height: 140,
-              width: 140,
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color: Colors.white,
-                    width: 8,
-                  ),
-                  image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage("assets/images/pic.png"))
-              ),
-            ),
-            Text("Hary",
-              style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.black
-              ),
-            ),
-            SizedBox(height: 5),
-            Text("harybaik@gmail.com",
-              style: TextStyle(
-                  fontWeight: FontWeight.w400,
-                  color: Color(0xFF8492A2)
-              ),),
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
             SizedBox(height: 40),
-            InkWell(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => Dasboard()));
-              },
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                child: Row(
-                  children: <Widget> [
-                    Icon(Icons.report),
-                    SizedBox(width: 20),
-                    Text(
-                      "Laporan",
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Color(0xFF7286A5)
-                      ),
-                    ),
-                    Spacer(),
-                    Icon(Icons.arrow_forward_ios,
-                        size: 16,
-                        color: Color(0xFF7286A5)
-                    )
-                  ],
+            CircleAvatar(
+              radius: 70,
+              backgroundImage: AssetImage("assets/images/pic.png"),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Hary",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "harybaik@gmail.com",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: Color(0xFF8492A2),
+              ),
+            ),
+            SizedBox(height: 40),
+            buildMenuItem(Icons.person, "Profil"),
+            buildMenuItem(Icons.settings, "Pengaturan"),
+            buildMenuItem(Icons.lock, "Keamanan"),
+            buildMenuItem(Icons.help, "Bantuan"),
+            SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              child: Text(
+                "Keluar",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
                 ),
               ),
             ),
-            InkWell(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => Dasboard()));
-              },
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                child: Row(
-                  children: <Widget> [
-                    Icon(Icons.report),
-                    SizedBox(width: 20),
-                    Text(
-                      "Laporan",
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Color(0xFF7286A5)
-                      ),
-                    ),
-                    Spacer(),
-                    Icon(Icons.arrow_forward_ios,
-                        size: 16,
-                        color: Color(0xFF7286A5)
-                    )
-                  ],
-                ),
-              ),
-            ),
-            InkWell(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => Dasboard()));
-              },
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                child: Row(
-                  children: <Widget> [
-                    Icon(Icons.report),
-                    SizedBox(width: 20),
-                    Text(
-                      "Laporan",
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Color(0xFF7286A5)
-                      ),
-                    ),
-                    Spacer(),
-                    Icon(Icons.arrow_forward_ios,
-                        size: 16,
-                        color: Color(0xFF7286A5)
-                    )
-                  ],
-                ),
-              ),
-            ),
+            SizedBox(height: 40),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget buildMenuItem(IconData icon, String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+      child: Row(
+        children: <Widget>[
+          Icon(
+            icon,
+            size: 24,
+            color: Color(0xFF7286A5),
+          ),
+          SizedBox(width: 20),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 20,
+              color: Color(0xFF7286A5),
+            ),
+          ),
+          Spacer(),
+          Icon(
+            Icons.arrow_forward_ios,
+            size: 16,
+            color: Color(0xFF7286A5),
+          ),
+        ],
       ),
     );
   }
