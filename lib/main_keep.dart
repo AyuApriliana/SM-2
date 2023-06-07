03fe43f3320219dff4df20b8ae97304d0cafecdc
AyuApriliana <aprilianaayu14@gmail.com>
2023-06-07 11:08:58 +0700

    image

---
+++ lib/main_keep.dart
@@ -0,0 +1,32 @@
+// import 'package:flutter/material.dart';
+// import 'package:laporan/card_picture.dart';
+// import 'package:provider/provider.dart';
+// import 'package:laporan/beranda.dart';
+// import 'package:laporan/home_page.dart';
+// import 'package:laporan/sidebar.dart';
+
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return  // Buat instance LaporanData
+//     MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         home: Scaffold(
+//           // drawer: Navbar(),
+//           // appBar: AppBar(
+//           //   title: const Text('Jalan Mulus'),
+//           //   backgroundColor: Colors.lightBlueAccent,
+//           // ),
+//           body: CardPicture(),
+//         ),
+//       );
+//   }
+// }
